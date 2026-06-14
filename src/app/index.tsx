c16329c655e56f4dcd1ba1bd957e44460699ed17
Bittu Kumar <bk7355583@gmail.com>
2026-06-14 21:36:21 +0530

    secure storage

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,201 +1,48 @@
-import { Text, StyleSheet, Button } from "react-native";
-import AsyncStorage from "@react-native-async-storage/async-storage";
-import { useState } from "react";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import * as SecureStore from 'expo-secure-store'
+import { Button } from '@react-navigation/elements'
 
-export default function Index() {
-
-  const [data, setData] = useState("");
-
-
-  const myObject={
-    name:"Bittu",
-    age:22
-
+const index = () => {
+  const [output, setOutput] = React.useState('')
+  const saveToken = async () => {
+    await SecureStore.setItemAsync('token', '123abc')
   }
-
-  // Store Data
-  const storeData = async () => {
-    try {
-      await AsyncStorage.setItem("user", "Bittu");
-      console.log("Data Stored");
-    } catch (e) {
-      console.log(e);
-    }
-  };
-
-
-  // Get Data
-  const getData = async () => {
-    try {
-      const value = await AsyncStorage.getItem("user");
-
-      if (value !== null) {
-        setData(value);
-      } else {
-        setData("No Data Found");
-      }
-
-    } catch (e) {
-      console.log(e);
-    }
-  };
-
-
-  // Remove Single Item
-  const removeData = async () => {
-    try {
-      await AsyncStorage.removeItem("user");
-      setData("");
-      console.log("Removed");
-    } catch (e) {
-      console.log(e);
-    }
-  };
-
-
-  // Clear All Data
-  const clearData = async () => {
-    try {
-      await AsyncStorage.clear();
-      setData("");
-      console.log("All Data Cleared");
-    } catch (e) {
-      console.log(e);
-    }
-  };
-
-
-  // Multi Set
-  const multiSet = async () => {
-    try {
-      await AsyncStorage.multiSet([
-        ["user", "Bittu"],
-        ["user2", "Bittu2"]
-      ]);
-
-      console.log("Multiple Data Stored");
-
-    } catch (e) {
-      console.log(e);
-    }
-  };
-
-
-  // Multi Get
-  const multiGet = async () => {
-    try {
-
-      const keys = ["user", "user2"];
-
-      const values = await AsyncStorage.multiGet(keys);
-
-      console.log(values);
-
-      setData(
-        values
-          .map(item => `${item[0]} : ${item[1]}`)
-          .join("\n")
-      );
-
-    } catch (e) {
-      console.log(e);
-    }
-  };
-
-  const setObject=async()=>{
-    try {
-      await AsyncStorage.setItem("user",JSON.stringify(myObject));
-      console.log("Data Stored");
-    } catch (e) {
-      console.log(e);
-    }
+  const getToken = async () => {
+    const token = await SecureStore.getItemAsync('token')
+    setOutput(token!)
+    console.log(token)
   }
-
-  //get object
-  const getObject=async()=>{
-    try {
-      const value = await AsyncStorage.getItem("user");
-      setData(JSON.stringify(value));
-      console.log(value);
-    } catch (e) {
-      console.log(e);
-    }
+  const removeToken = async () => {
+    await SecureStore.deleteItemAsync('token')
+    setOutput('')
   }
-
-
+  const checkAvailability = async () => {
+    const result = await SecureStore.isAvailableAsync()
+    setOutput(result ? 'Available' : 'Not Available')
+  }
+  const setObject = async () => {
+    await SecureStore.setItemAsync('user', JSON.stringify({ name: 'John Doe', age: 30 }))
+  }
+  const getObject = async () => {
+    const user = await SecureStore.getItemAsync('user')
+    setOutput(user!)
+  }
   return (
-    <SafeAreaView style={styles.container}>
+    <View style={{ flex: 1, alignItems: 'center',gap:10 }}>
+      <Text style={{ fontSize: 20, fontWeight: 'bold',marginTop:20 }}>Secure Storage</Text>
+      <Button onPress={saveToken}>Save Token</Button>
+      <Button onPress={getToken}>Get Token</Button>
+      <Button onPress={removeToken}>Remove Token</Button>
+      <Button onPress={checkAvailability}>Check Availability</Button>
+      <Button onPress={setObject}>Set Object</Button>
+      <Button onPress={getObject}>Get Object</Button>
+      <Text> Output :{output}</Text>
 
-      <Button 
-        title="Store Data" 
-        onPress={storeData} 
-      />
-
-      <Button 
-        title="Get Data" 
-        onPress={getData} 
-      />
-
-      <Button 
-        title="Remove Data" 
-        onPress={removeData} 
-      />
-
-      <Button 
-        title="Clear All" 
-        onPress={clearData} 
-      />
-
-      <Button 
-        title="Multi Set" 
-        onPress={multiSet} 
-      />
-
-      <Button 
-        title="Multi Get" 
-        onPress={multiGet} 
-      />
-
-      <Button 
-        title="Set Object" 
-        onPress={setObject} 
-      />
-
-      <Button 
-        title="Get Object" 
-        onPress={getObject} 
-      />
-
-
-      <Text style={styles.text}>
-        Output:
-      </Text>
-
-      <Text style={styles.text}>
-        {data}
-      </Text>
-
-
-    </SafeAreaView>
-  );
+    </View>
+  )
 }
 
+export default index
 
-const styles = StyleSheet.create({
-
-  container:{
-    flex:1,
-    alignItems:"center",
-    justifyContent:"center",
-    backgroundColor:"#fff",
-    padding:20,
-    gap:10
-  },
-
-  text:{
-    fontSize:18,
-    marginTop:10
-  }
-
-});
+const styles = StyleSheet.create({})
