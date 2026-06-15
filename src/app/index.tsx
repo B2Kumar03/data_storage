b95d0bdd872a53e78970e706866fe1dcaffa889b
Bittu Kumar <bk7355583@gmail.com>
2026-06-15 22:13:29 +0530

    file handling added.

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,150 +1,391 @@
-import { Button, Text } from 'react-native'
-import React from 'react'
-import { SafeAreaView } from 'react-native-safe-area-context'
-import * as SQLite from 'expo-sqlite'
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  ScrollView
+} from 'react-native'
 
-const db = SQLite.openDatabaseSync('db.db')
+import React, { useState } from 'react'
+import { File, Directory, Paths } from 'expo-file-system'
 
-const SQlite = () => {
-  const [output , setOutput] = React.useState<string>('')
 
-  const createTable = () => {
-    db.execSync(`
-      CREATE TABLE IF NOT EXISTS users (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        name TEXT,
-        email TEXT,
-        password TEXT
-      );
-    `)
-    setOutput("Table created")
+const demoFile = new File(
+  Paths.document,
+  "demo.txt"
+)
 
-    console.log("Table created")
+
+const copiedFile = new File(
+  Paths.document,
+  "copied.txt"
+)
+
+
+const movedFile = new File(
+  Paths.document,
+  "moved.txt"
+)
+
+
+const notesDirectory = new Directory(
+  Paths.document,
+  "notes"
+)
+
+
+
+const index = () => {
+
+  const [output,setOutput] = useState("")
+  const [downloadUri,setDownloadUri] = useState("")
+
+
+  // CREATE / WRITE FILE
+
+  const writeFile = async()=>{
+
+    await demoFile.write("Hello World")
+
+    setOutput("File created")
   }
 
 
-  const insertUser = () => {
-//before insertion check the table availability
 
-    db.execSync(`
-      CREATE TABLE IF NOT EXISTS users (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        name TEXT,
-        email TEXT,
-        password TEXT
-      );
-    `)
+  // READ FILE
 
-    const result = db.runSync(
-      'INSERT INTO users (name, email, password) VALUES (?, ?, ?)',
-      [
-        "bittu",
-        "bittu@gmail.com",
-        "1234"
-      ]
+  const readFile = async()=>{
+
+    const data = await demoFile.text()
+
+    setOutput(data)
+
+  }
+
+
+
+  // APPEND FILE
+
+  const appendFile = async()=>{
+
+
+    const oldData = await demoFile.text()
+
+
+    await demoFile.write(
+      oldData + "\nB Hello World"
     )
 
-    setOutput("Inserted ID: " + result.lastInsertRowId)
 
-    console.log("Inserted ID:", result.lastInsertRowId)
-  }
-
-
-
-  const selectUser = () => {
-
-    const users = db.getAllSync(
-      'SELECT * FROM users'
+    setOutput(
+      oldData + "\nB Hello World"
     )
-    console.log(users)
-
-    setOutput(JSON.stringify(users))
 
   }
 
-//clear table
-  const clearTable = () => {
-    db.runSync('DELETE FROM users')
-    setOutput("Table cleared")
+
+
+
+  // DELETE FILE
+
+  const deleteFile = async()=>{
+
+    await demoFile.delete()
+
+    setOutput("File deleted")
+
   }
 
-  const update=() => {
-    db.runSync('UPDATE users SET name = ?, email = ?, password = ? WHERE id = ?',
-    [
-      "bittuKumsri",
-      "bittu@gmail.com",
-      "1234",
-      1
-    ]
+
+
+
+  // COPY FILE
+
+  const copyFile = async()=>{
+
+
+    await demoFile.copy(copiedFile)
+
+
+    setOutput("File copied")
+
+  }
+
+
+
+
+  // MOVE FILE
+
+  const moveFileHandler = async()=>{
+
+
+    await copiedFile.move(movedFile)
+
+
+    setOutput("File moved")
+
+  }
+
+
+
+
+
+  // FILE INFO
+
+  const getFileInfo = ()=>{
+
+
+    setOutput(
+
+      JSON.stringify(
+        {
+          name:demoFile.name,
+          uri:demoFile.uri,
+          size:demoFile.size,
+          exists:demoFile.exists
+        },
+        null,
+        2
+      )
+
     )
-    setOutput("Table updated")
+
   }
 
-  //drop table
-  const dropTable = () => {
-    db.runSync('DROP TABLE users')
-    setOutput("Table dropped")
+
+
+
+
+  // DELETE ALL
+
+  const deleteAllFile = ()=>{
+
+
+     demoFile.delete()
+     copiedFile.delete()
+     movedFile.delete()
+
+
+    setOutput("All files deleted")
+
   }
 
-  return (
-    <SafeAreaView 
-      style={{
-        flex:1,
-        alignItems:'center',
-        justifyContent:'center',
-        gap:20
-      }}
-    >
-
-      <Text style={{fontSize:30,fontWeight:'bold'}}>
-        SQLite Database
-      </Text>
 
 
-      <Button 
-        title="Create Table" 
-        onPress={createTable}
-      />
+
+  // CREATE FOLDER
 
 
-      <Button 
-        title="Insert User" 
-        onPress={insertUser}
-      />
+  const createFolder = async()=>{
 
 
-      <Button 
-        title="Select User" 
-        onPress={selectUser}
-      />
+    await notesDirectory.create()
 
 
-      <Button 
-        title="Clear Table" 
-        onPress={clearTable}
-      />
+    setOutput("Folder created")
+
+  }
 
 
-      <Button 
-        title="Update User" 
-        onPress={update}
-      />
 
 
-      <Button 
-        title="Drop Table" 
-        onPress={dropTable}
-      />
+
+  // READ DIRECTORY
 
 
-      <Text>
-        Output:
-        {output}
-      </Text>
+  const readDir = async()=>{
 
-    </SafeAreaView>
-  )
+
+    const files = await notesDirectory.list()
+
+
+    setOutput(
+
+      JSON.stringify(
+        files.map(
+          item=>item.uri
+        ),
+        null,
+        2
+      )
+
+    )
+
+
+  }
+
+
+
+
+
+  // DOWNLOAD FILE
+
+
+  const downloadFile = async()=>{
+
+
+    const folder = new Directory(
+      Paths.cache,
+      "images"
+    )
+
+
+    await folder.create()
+
+
+
+    const downloadedFile =
+      await File.downloadFileAsync(
+        "https://picsum.photos/300",
+        folder
+      )
+
+
+
+    setDownloadUri(downloadedFile.uri)
+
+
+    setOutput(
+
+      JSON.stringify(
+        {
+          uri:downloadedFile.uri,
+          size:downloadedFile.size,
+          exists:downloadedFile.exists
+        },
+        null,
+        2
+      )
+
+    )
+
+  }
+
+
+
+
+return (
+
+<ScrollView>
+
+<View style={styles.container}>
+
+
+<Text style={styles.title}>
+Expo File System Demo
+</Text>
+
+
+
+<Button
+title="Write File"
+onPress={writeFile}
+/>
+
+
+<Button
+title="Read File"
+onPress={readFile}
+/>
+
+
+<Button
+title="Append File"
+onPress={appendFile}
+/>
+
+
+<Button
+title="Delete File"
+onPress={deleteFile}
+/>
+
+
+<Button
+title="Copy File"
+onPress={copyFile}
+/>
+
+
+<Button
+title="Move File"
+onPress={moveFileHandler}
+/>
+
+
+<Button
+title="File Info"
+onPress={getFileInfo}
+/>
+
+
+<Button
+title="Delete All Files"
+onPress={deleteAllFile}
+/>
+
+
+<Button
+title="Create Folder"
+onPress={createFolder}
+/>
+
+
+<Button
+title="Read Folder"
+onPress={readDir}
+/>
+
+
+<Button
+title="Download Image"
+onPress={downloadFile}
+/>
+
+
+
+<Text style={styles.output}>
+{output}
+</Text>
+
+
+<Text>
+Download URI:
+{downloadUri}
+</Text>
+
+
+
+</View>
+
+
+</ScrollView>
+
+)
+
 }
 
 
-export default SQlite
+export default index
+
+
+
+const styles = StyleSheet.create({
+
+container:{
+padding:20,
+gap:15
+},
+
+title:{
+fontSize:22,
+fontWeight:"bold",
+marginBottom:20
+},
+
+output:{
+marginTop:20,
+fontSize:16
+}
+
+})
